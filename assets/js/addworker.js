03ebb233bb012e67d3b949b706bc3784193a2a3c
nazlyrazack <nazly@bromlays.com>
2018-09-12 16:53:06 +0530

    add worker validation

--- assets/js/addworker.js
+++ assets/js/addworker.js
@@ -19,7 +19,10 @@ var district = $('#district').val();
 var state = $('#state').val();
 var latitude = $('#latitude').val();
 var longitude = $('#longitude').val();
+var rate = $('#rate').val();
+var exp = $('#exp').val();
 var gender =  $("input[name=inlineRadioOptions]:checked").length;
+var noOfSelectedItems=$('#lstBox2 :selected').length;
 // Initializing Variables With Regular Expressions
 var name_regex = /^[a-zA-Z]+$/;
 var email_regex = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;;
@@ -145,6 +148,18 @@ $('#p17').text("* For your state please use alphabets only *"); // This Segment 
 $("#state").focus();
 return false;
 }
+
+else if(noOfSelectedItems==0)
+{
+$('#p20').text("* select category *"); // This Segment Displays The Validation Rule For Name
+$("#lstBox2").focus();
+return false;
+}
+else if (!rate.match(name_regex) || rate.length == 0) {
+$('#p21').text("* For your state please use alphabets only *"); // This Segment Displays The Validation Rule For Name
+$("#state").focus();
+return false;
+}
 else {
 alert("Form Submitted Successfuly!");
 return true;
@@ -152,167 +167,3 @@ return true;
 });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function(){
-// 	$('#add_user').bootstrapValidator({
-// 		container:'#messages',
-// 		feedbackIcons: {
-// 			valid: 'glyphicon glyphicon-ok',
-//             invalid: 'glyphicon glyphicon-remove',
-//             validating: 'glyphicon glyphicon-refresh'
-// 		},
-// 		fields:{
-// 			username: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The username is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			firstname: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The firstname is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			lastname: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The lastname is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			email: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The email is required and cannot be empty'
-// 					},
-// 					emailAddress: {
-// 						message: 'The email address is not valid'
-// 					}
-// 				}
-// 			},
-// 			password: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The password is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			confirm_password: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The confirm password is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			age: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The age is required and cannot be empty'
-// 					},
-// 					stringLength: {
-//                         max: 3,
-//                         message: 'The age must be less than 3 characters long'
-//                     }
-// 				}
-// 			},
-// 			address1: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The address1 is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			phone: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The phone number is required and cannot be empty'
-// 					},
-// 					stringLength: {
-//                         max: 10,
-//                         message: 'The number must be 10 characters long'
-//                     }
-// 				}
-// 			},
-// 			location: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The location is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			sublocality: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The sub locality is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			landmark: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The landmark is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			city: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The city is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			district: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The district is required and cannot be empty'
-// 					}
-// 				}
-// 			},
-// 			state: {
-// 				validators: {
-// 					notEmpty: {
-// 						message: 'The state is required and cannot be empty'
-// 					}
-// 				}
-// 			}
-			
-// 		}
-// 	});
-// 	});
